f6dfba25071cb0c849bc8507cb9261b1e5e1cbaa
Quinten Clause <quintenclause@hotmail.com>
2021-03-04 20:09:20 +0100

    Minify css

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,17 @@
-module.exports = {
-    dir: {
-        input: "data/pages",
-        output: "public",
-        layouts: "../../layouts",
-        data: "../config"
-    }
+const CleanCSS = require('clean-css');
+
+module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addFilter('cssmin', function(code) {
+        return new CleanCSS({}).minify(code).styles;
+    });
+
+    return {
+        dir: {
+            input: "data/pages",
+            output: "public",
+            layouts: "../../layouts",
+            data: "../config"
+        }
+    };
 };
