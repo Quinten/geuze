4d9f52930a67cbb127fd50841eda33fdadfe89b2
Quinten Clause <quintenclause@hotmail.com>
2021-03-08 10:45:57 +0100

    Fix pancake

--- .eleventy.js
+++ .eleventy.js
@@ -12,11 +12,11 @@ module.exports = function(eleventyConfig) {
         return `<div iframe="responsive"><iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>`;
     });
 
-    eleventyConfig.addShortcode('pancake', function(contents) {
-        return `<div pancake="decontsturcted">${contents}</div>`;
+    eleventyConfig.addPairedShortcode('pancake', function(contents) {
+        return `<div pancake="deconstructed">${contents}</div>`;
     });
 
-    eleventyConfig.addShortcode('slice', function(contents) {
+    eleventyConfig.addPairedShortcode('slice', function(contents) {
         return `<div>${contents}</div>`;
     });
 
