0d3475515b14293255f5cbee9d44e9c48aaf76bb
Quinten Clause <quintenclause@hotmail.com>
2021-03-14 22:40:33 +0100

    Add comments function

---
+++ netlify/functions/comments.js
@@ -0,0 +1,57 @@
+let https = require('https');
+
+let getJson = (url) => {
+    return new Promise((resFunc, rejFunc) => {
+        https.get(url, res => {
+            res.setEncoding('utf8');
+            let body = '';
+            res.on('data', data => {
+                body += data;
+            });
+            res.on('end', () => {
+                body = JSON.parse(body);
+                resFunc(body);
+            });
+        });
+    });
+};
+
+let handler = async (event, context) => {
+
+    if (process.env.NETLIFY_ACCESS_TOKEN === undefined) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify([])
+        };
+    }
+
+    if (process.env.SITE_ID === undefined) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify([])
+        };
+    }
+
+    let forms = await getJson(`https://api.netlify.com/api/v1/sites/${process.env.SITE_ID}/forms?access_token=${process.env.NETLIFY_ACCESS_TOKEN}`);
+
+    let form_id = undefined;
+    forms.forEach(form => {
+        if (form.name === 'comments') {
+            form_id = form.id;
+        }
+    });
+    if (form_id === undefined) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify([])
+        };
+    }
+
+    let comments = await getJson(`https://api.netlify.com/api/v1/forms/${form_id}/submissions?access_token=${process.env.NETLIFY_ACCESS_TOKEN}`);
+    return {
+        statusCode: 200,
+        body: JSON.stringify(comments)
+    };
+};
+
+exports.handler = handler;
