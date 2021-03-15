450da3a4470dd81f1927e78f851560978b6d8c01
Quinten Clause <quintenclause@hotmail.com>
2021-03-15 11:27:45 +0100

    Debug deletecomment

---
+++ netlify/functions/deletecomment.js
@@ -0,0 +1,58 @@
+let https = require('https');
+
+let deleteCall = (url) => {
+    return new Promise((resFunc, rejFunc) => {
+        https.request(url, {method: 'DELETE'}, res => {
+            res.setEncoding('utf8');
+            let body = '';
+            res.on('data', data => {
+                body += data;
+            });
+            res.on('end', () => {
+                body = JSON.parse(body);
+                resFunc(body);
+            });
+        });
+    });
+};
+
+let handler = async (event, context) => {
+
+    if (process.env.NETLIFY_ACCESS_TOKEN === undefined) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({})
+        };
+    }
+
+    if (process.env.SITE_ID === undefined) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({})
+        };
+    }
+
+    let {user} = context.clientContext;
+    return {
+        statusCode: 200,
+        body: JSON.stringify(user)
+    };
+
+
+    let {id} = event.queryStringParameters;
+    if (id === undefined) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({})
+        };
+    }
+
+    let response = await deleteCall(`https://api.netlify.com/api/v1/submissions/${id}?access_token=${process.env.NETLIFY_ACCESS_TOKEN}`);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({success: true})
+    };
+};
+
+exports.handler = handler;
