b208fe30c93d924bb34683e22df8b1730bd7ad47
Quinten Clause <quintenclause@hotmail.com>
2021-03-15 09:57:59 +0100

    Fix comments

--- public/js/comments.js
+++ public/js/comments.js
@@ -9,13 +9,15 @@ if (window.location.search.indexOf('success=yes') > -1) {
     flashmessage(form.getAttribute('success-message'));
 }
 
-fetch('/.netlify/functions/comments?slug=' + form.getAttribute('name').replace('commments-', ''))
+fetch('/.netlify/functions/comments?slug=' + form.getAttribute('name').replace('comments-', ''))
     .then(response => response.json())
     .then(comments => {
-        let html = '';
-        comments.forEach(comment => {
-            html = html + `<div card="true"><p><strong>${comment.name}</strong>:</p><p><em>${(new Date(comment.created_at)).toLocaleDateString('en', {year: 'numeric', month: 'long', day: 'numeric'})}</em></p><p>${comment.comment}</p></div>`;
-        });
-        document.getElementById('comments-list').innerHTML = html;
+        if (comments && comments.length) {
+            let html = '';
+            comments.forEach(comment => {
+                html = html + `<div card="true"><p><strong>${comment.name}</strong>:</p><p><em>${(new Date(comment.created_at)).toLocaleDateString('en', {year: 'numeric', month: 'long', day: 'numeric'})}</em></p><p>${comment.comment}</p></div>`;
+            });
+            document.getElementById('comments-list').innerHTML = html;
+        }
     })
     .catch(console.error);
